c771f0f17a37d9b558b1265f1b17d9971304e95a
Martin Krychtálek <93790230+mkrychtalek@users.noreply.github.com>
2023-08-09 14:49:48 +0200

    update copy

--- copy.js
+++ copy.js
@@ -1,5 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var copyLink = document.getElementById("copyLink");
+    var copyLink = document.getElementById("copyLink1");
+
+    copyLink.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        var textToCopy = copyLink.innerText;
+
+        navigator.clipboard.writeText(textToCopy).then(function() {
+            console.log("Text copied to clipboard:", textToCopy);
+            alert("URL copied to clipboard: " + textToCopy);
+        }).catch(function(error) {
+            console.error("Unable to copy text to clipboard:", error);
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var copyLink = document.getElementById("copyLink2");
 
     copyLink.addEventListener("click", function(event) {
         event.preventDefault();

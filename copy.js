a5c6c8da0a835df0499eb7481821e69b2a1cb4cf
Martin Krychtálek <93790230+xdwsdev@users.noreply.github.com>
2024-05-07 22:32:30 +0200

    ui overhaul

--- copy.js
+++ copy.js
@@ -14,20 +14,3 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
-
-document.addEventListener("DOMContentLoaded", function() {
-    var copyLink = document.getElementById("copyLink2");
-
-    copyLink.addEventListener("click", function(event) {
-        event.preventDefault();
-
-        var textToCopy = copyLink.innerText;
-
-        navigator.clipboard.writeText(textToCopy).then(function() {
-            console.log("Text copied to clipboard:", textToCopy);
-            alert("URL copied to clipboard: " + textToCopy);
-        }).catch(function(error) {
-            console.error("Unable to copy text to clipboard:", error);
-        });
-    });
-});
---
+++ previous-versions/copy.js
@@ -0,0 +1,186 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var copyLink = document.getElementById("copyLink1.1");
+
+    copyLink.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        var textToCopy = copyLink.innerText;
+
+        navigator.clipboard.writeText(textToCopy).then(function() {
+            console.log("Text copied to clipboard:", textToCopy);
+            alert("URL copied to clipboard: " + textToCopy);
+        }).catch(function(error) {
+            console.error("Unable to copy text to clipboard:", error);
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var copyLink = document.getElementById("copyLink1.2");
+
+    copyLink.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        var textToCopy = copyLink.innerText;
+
+        navigator.clipboard.writeText(textToCopy).then(function() {
+            console.log("Text copied to clipboard:", textToCopy);
+            alert("URL copied to clipboard: " + textToCopy);
+        }).catch(function(error) {
+            console.error("Unable to copy text to clipboard:", error);
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var copyLink = document.getElementById("copyLink2");
+
+    copyLink.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        var textToCopy = copyLink.innerText;
+
+        navigator.clipboard.writeText(textToCopy).then(function() {
+            console.log("Text copied to clipboard:", textToCopy);
+            alert("URL copied to clipboard: " + textToCopy);
+        }).catch(function(error) {
+            console.error("Unable to copy text to clipboard:", error);
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var copyLink = document.getElementById("copyLink3");
+
+    copyLink.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        var textToCopy = copyLink.innerText;
+
+        navigator.clipboard.writeText(textToCopy).then(function() {
+            console.log("Text copied to clipboard:", textToCopy);
+            alert("URL copied to clipboard: " + textToCopy);
+        }).catch(function(error) {
+            console.error("Unable to copy text to clipboard:", error);
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var copyLink = document.getElementById("copyLink4");
+
+    copyLink.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        var textToCopy = copyLink.innerText;
+
+        navigator.clipboard.writeText(textToCopy).then(function() {
+            console.log("Text copied to clipboard:", textToCopy);
+            alert("URL copied to clipboard: " + textToCopy);
+        }).catch(function(error) {
+            console.error("Unable to copy text to clipboard:", error);
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var copyLink = document.getElementById("copyLink5");
+
+    copyLink.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        var textToCopy = copyLink.innerText;
+
+        navigator.clipboard.writeText(textToCopy).then(function() {
+            console.log("Text copied to clipboard:", textToCopy);
+            alert("URL copied to clipboard: " + textToCopy);
+        }).catch(function(error) {
+            console.error("Unable to copy text to clipboard:", error);
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var copyLink = document.getElementById("copyLink6");
+
+    copyLink.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        var textToCopy = copyLink.innerText;
+
+        navigator.clipboard.writeText(textToCopy).then(function() {
+            console.log("Text copied to clipboard:", textToCopy);
+            alert("URL copied to clipboard: " + textToCopy);
+        }).catch(function(error) {
+            console.error("Unable to copy text to clipboard:", error);
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var copyLink = document.getElementById("copyLink7");
+
+    copyLink.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        var textToCopy = copyLink.innerText;
+
+        navigator.clipboard.writeText(textToCopy).then(function() {
+            console.log("Text copied to clipboard:", textToCopy);
+            alert("URL copied to clipboard: " + textToCopy);
+        }).catch(function(error) {
+            console.error("Unable to copy text to clipboard:", error);
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var copyLink = document.getElementById("copyLink8");
+
+    copyLink.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        var textToCopy = copyLink.innerText;
+
+        navigator.clipboard.writeText(textToCopy).then(function() {
+            console.log("Text copied to clipboard:", textToCopy);
+            alert("URL copied to clipboard: " + textToCopy);
+        }).catch(function(error) {
+            console.error("Unable to copy text to clipboard:", error);
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var copyLink = document.getElementById("copyLink9");
+
+    copyLink.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        var textToCopy = copyLink.innerText;
+
+        navigator.clipboard.writeText(textToCopy).then(function() {
+            console.log("Text copied to clipboard:", textToCopy);
+            alert("URL copied to clipboard: " + textToCopy);
+        }).catch(function(error) {
+            console.error("Unable to copy text to clipboard:", error);
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var copyLink = document.getElementById("copyLink10");
+
+    copyLink.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        var textToCopy = copyLink.innerText;
+
+        navigator.clipboard.writeText(textToCopy).then(function() {
+            console.log("Text copied to clipboard:", textToCopy);
+            alert("URL copied to clipboard: " + textToCopy);
+        }).catch(function(error) {
+            console.error("Unable to copy text to clipboard:", error);
+        });
+    });
+});
